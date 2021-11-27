39096f9684f1ac5f272ff6e91576f0ba6963efaa
daniel <dancrhorton@gmail.com>
2021-11-26 21:02:49 -0500

    d11 part 2

---
+++ y21d9/src/lib.rs
@@ -0,0 +1,16 @@
+use helper::{print_part_1, print_part_2};
+
+const FILENAME: &str = env!("CARGO_PKG_NAME");
+
+fn main() {
+
+}
+
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn it_works() {
+        assert_eq!(2 + 2, 4);
+    }
+}
