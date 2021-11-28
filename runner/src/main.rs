3c2ba5dcb63101ccec113c49d8da31714c001f1e
daniel <dancrhorton@gmail.com>
2021-11-27 20:06:41 -0500

    run just 1 day

--- runner/src/main.rs
+++ runner/src/main.rs
@@ -1,45 +1,52 @@
+use std::env;
 use std::time::Instant;
+
+const FNS: [fn(); 25] = [
+    y21d1::main,
+    y21d2::main,
+    y21d3::main,
+    y21d4::main,
+    y21d5::main,
+    y21d6::main,
+    y21d7::main,
+    y21d8::main,
+    y21d9::main,
+    y21d10::main,
+    y21d11::main,
+    y21d12::main,
+    y21d13::main,
+    y21d14::main,
+    y21d15::main,
+    y21d16::main,
+    y21d17::main,
+    y21d18::main,
+    y21d19::main,
+    y21d20::main,
+    y21d21::main,
+    y21d22::main,
+    y21d23::main,
+    y21d24::main,
+    y21d25::main,
+];
 
 fn main() {
     let start_0 = Instant::now();
 
-    run_libs();
+    let args: Vec<String> = env::args().collect();
+    if args.len() < 2 {
+        run_libs();
+    } else {
+        let day = args[1].parse::<usize>().unwrap();
+        run_lib(FNS[day - 1], day)
+    }
 
     let stop = start_0.elapsed().as_micros() as f64 / 1000.0;
     println!("Total Duration: {:.3}ms", stop);
 }
 
 fn run_libs() {
-    let fns = [
-        y21d1::main,
-        y21d2::main,
-        y21d3::main,
-        y21d4::main,
-        y21d5::main,
-        y21d6::main,
-        y21d7::main,
-        y21d8::main,
-        y21d9::main,
-        y21d10::main,
-        y21d11::main,
-        y21d12::main,
-        y21d13::main,
-        y21d14::main,
-        y21d15::main,
-        y21d16::main,
-        y21d17::main,
-        y21d18::main,
-        y21d19::main,
-        y21d20::main,
-        y21d21::main,
-        y21d22::main,
-        y21d23::main,
-        y21d24::main,
-        y21d25::main
-    ];
-
-    for (i,f) in fns.iter().enumerate() {
-        run_lib(f,i+1)
+    for (i, f) in FNS.iter().enumerate() {
+        run_lib(f, i + 1)
     }
 }
 
