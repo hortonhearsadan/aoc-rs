41fe51883d023ae25185d165223d529f4b252529
danielh <daniel.horton@broadsign.com>
2021-12-01 08:41:11 -0500

    alternate

--- y21d1/src/lib.rs
+++ y21d1/src/lib.rs
@@ -12,12 +12,39 @@ pub fn main() {
     let count_1 = count_increases(&inputs, 1);
     let count_2 = count_increases(&inputs, 3);
 
+    // let (count_1,count_2) = count_increases_3(&inputs);
+
     print_part_1(count_1);
     print_part_2(count_2);
 }
 
 fn count_increases(depths: &[i32], step: usize) -> usize {
     depths.windows(step + 1).filter(|w| w[0] < w[step]).count()
+}
+
+fn count_increases_2(depths: &[i32], step: usize) -> usize {
+    depths
+        .iter()
+        .zip(depths[step..].iter())
+        .filter(|(x, y)| x < y)
+        .count()
+}
+
+fn count_increases_3(depths: &[i32]) -> (usize, usize) {
+    let mut count_1 = 0;
+    let mut count_2 = 0;
+    let l = depths.len();
+    for (i, d) in depths.iter().enumerate() {
+        let j = i + 1;
+        let k = i + 3;
+        if j < l && d < &depths[j] {
+            count_1 += 1
+        }
+        if k < l && d < &depths[k] {
+            count_2 += 1
+        }
+    }
+    (count_1, count_2)
 }
 
 #[cfg(test)]
